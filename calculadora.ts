b748a4256fbbf547165513b342db74e156db0f42
lgalan <lein.galan@sine95.com>
2026-01-12 16:39:07 -0400

    feat: add multiplication function

--- calculadora.ts
+++ calculadora.ts
@@ -1,5 +1,6 @@
 const sumar = (a: number, b: number): number => a + b;
 const restar = (a: number, b: number): number => a - b;
 const dividir = (a: number, b: number): number => a / b;
+const multiplicar = (a: number, b: number): number => a * b;
 
 console.log(sumar(10, 5));
